50d76082c3f07288ed31d4ec75aa9d6a9a73c0d1
q9002285117 <105021038@live.asia.edu.tw>
2016-10-30 13:32:31 +0800

    hw

--- src/hw/hw05.java
+++ src/hw/hw05.java
@@ -1,10 +1,15 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw05 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int data[][] = new int[n][n];
+		data[0][0] = 1;
 	}
 
 }
